5f32c31795516bba67ead6d3d9611659db53126b
juro44ka <128833645+juro44ka@users.noreply.github.com>
2024-02-29 18:07:24 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+def replace_elements(arr, Z):
+  count = 0
+  for i in range(len(arr)):
+      if arr[i] > Z:
+          arr[i] = Z
+          count += 1
+  return count
+
+n = int(input("Введіть розмір послідовності: "))
+arr = []
+
+print("Введіть елементи послідовності:")
+for l in range(n):
+  arr.append(float(input()))
+
+Z = float(input("Введіть число Z: "))
+
+replacements = replace_elements(arr, Z)
+
+print("Масив після заміни:")
+print(' '.join(map(str, arr)))
+print("Кількість замін:", replacements)
